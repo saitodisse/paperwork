6ae7a5d1eca5de5f800deb8f83052dd9645ba2f3
Julio Makdisse Saito <saitodisse@gmail.com>
2015-08-19 15:42:03 -0300

    [azk] Fixing provision

--- Azkfile.js
+++ Azkfile.js
@@ -7,22 +7,32 @@ systems({
     // Dependent systems
     depends: ['db'],
     // More images:  http://images.azk.io
-    image: { dockerfile: "./frontend/Dokerfile" },
+    image: { docker: "azukiapp/php-fpm:5.6" },
     // Steps to execute before running instances
     provision: [
-      "php artisan migrate"
+      // "sed -i 's/return $app;//' bootstrap/start.php",
+      // "echo '$env = $app->detectEnvironment(function() { return \"development\"; }); return $app;' >> bootstrap/start.php",
+      "find ./app/storage -type d -print0 | xargs -0 chmod 0755",
+      "find ./app/storage -type f -print0 | xargs -0 chmod 0644",
+      // "npm update",
+      "npm install",
+      "node_modules/.bin/bower --allow-root install",
+      "node_modules/.bin/gulp",
+      // run migrations
+      "php artisan migrate",
     ],
     workdir: "/azk/#{manifest.dir}/#{system.name}",
     shell: "/bin/bash",
     wait: 20,
     mounts: {
       '/azk/#{manifest.dir}/#{system.name}': sync("./#{system.name}"),
+      '/azk/#{manifest.dir}/#{system.name}/vendor': persistent("#{system.name}/vendor"),
       '/azk/#{manifest.dir}/#{system.name}/app/storage': persistent("#{system.name}/app/storage"),
       '/azk/#{manifest.dir}/#{system.name}/app/js/bower_components': persistent("#{system.name}/app/js/bower_components"),
       '/azk/#{manifest.dir}/#{system.name}/node_modules': persistent("#{system.name}/node_modules"),
-      '/azk/#{manifest.dir}/#{system.name}/composer.phar': persistent("#{system.name}/composer.phar"),
+      // '/azk/#{manifest.dir}/#{system.name}/composer.phar': persistent("#{system.name}/composer.phar"),
       '/azk/#{manifest.dir}/#{system.name}/composer.lock': path("#{system.name}/composer.lock"),
-      '/azk/#{manifest.dir}/#{system.name}/.env.php': path("#{system.name}/.env.php"),
+      // '/azk/#{manifest.dir}/#{system.name}/.env.php': path("#{system.name}/.env.php"),
       '/azk/#{manifest.dir}/#{system.name}/bootstrap/compiled.php': path("#{system.name}/bootstrap/compiled.php"),
     },
     scalable: {"default": 1},
