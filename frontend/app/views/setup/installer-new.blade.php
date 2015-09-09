262b5f33e16213365190a3f36c03fdc63f909867
Liongold <beimaginativeegroup@gmail.com>
2015-09-09 16:34:40 +0000

    Third part

--- frontend/app/views/setup/installer-new.blade.php
+++ frontend/app/views/setup/installer-new.blade.php
@@ -42,7 +42,7 @@
         </div>
         <div style="left:0;right:0;z-index:1;display:block;width:100%;height:100%;/*overflow:hidden*/"> <!-- wizard -->
             <div style="height:10px"> <!-- progress container -->
-                <div style="width:0;background:red;"><!-- progress bar --></div>
+                <div id="progress_bar" style="width:0;background:red;height:10px;"><!-- progress bar --></div>
             </div>
             <div class="container-fluid" style="display:table;width:100%;height:100%">
                 <div style="z-index:1111111111;display:table-cell;vertical-align:middle;padding:0px">
@@ -71,15 +71,37 @@
                                     </li>
                                     <li class="form-group hidden">
                                         <h1>Setting up the database</h1>
-                                        <div> <!--- first drop down - dbms choice -->
-                                            <div>MySQL <span class="caret"></span></div>
-                                            <div>Choice 2 <span class="caret"></span></div>
+                                        <div style="width:48%;height:100%;float:left;argin-right:10px"> <!--- first drop down - dbms choice -->
+                                            <a style="padding:20px 0;display:block">MySQL <span class="caret"></span></a><br>
+                                            <a style="padding:20px 0;display:block;font-style:italic;background:#CCCCCC">Choice 2 <span class="caret"></span></a>
                                         </div>
-                                        <div> <!-- second drop down - requirements and credentials form -->
+                                        <div style="width:50%;height:100%;float:left;"> <!-- second drop down - requirements and credentials form -->
                                             <div>
-                                                <p>Requirements: ...</p>
-                                                <form>
-                                                    <!-- form -->
+                                                <p>Requirements: ...</p> 
+                                                <form class="form-horizontal">
+                                                    <div class="form-group">
+                                                        <label for="inputServer" class="col-sm-2 control-label">Server</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="text" class="form-control" id="inputServer" placeholder="Server">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label for="inputUser" class="col-sm-2 control-label">Username</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="text" class="form-control" id="inputUser" placeholder="Username">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label for="inputPassword" class="col-sm-2 control-label">Password</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="text" class="form-control" id="inputPassword" placeholder="Password">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <div class="col-sm-12">
+                                                            <button class="btn btn-default" id="connection_check">Check Connection</button>
+                                                        </div>
+                                                    </div>
                                                 </form>
                                             </div>
                                         </div>
@@ -93,12 +115,14 @@
                                         <button class="btn btn-primary btn-lg next_step" id="step3">Next</button>
                                     </li>
                                     <li class="form-group hidden">
-                                        <h1>Registerign your first user account</h1>
+                                        <h1>Registering your first user account</h1>
                                         <p>This account will serve as the administrator. </p>
-                                        <button class="btn btn-primary btn-lg next_step" id="step4">Next</button>
+                                        @include("partials/registration-form", array('back' => false, 'frominstaller' => true))
+                                        <!--<button class="btn btn-primary btn-lg next_step" id="step4">Next</button>-->
                                     </li>
                                     <li class="form-group hidden">
                                         <h1>Installation completed</h1>
+                                        <p>Congratulations! You have now finished installing and setting up Paperwork. Click on the link below to login in the account you have just created. </p>
                                         <button class="btn btn-primary btn-lg next_step" id="step5">Proceed to Paperwork</button>
                                     </li>
                                 </ul>
@@ -113,7 +137,7 @@
             $(".next_step").click(function(event) {
                 console.log(event);
                 console.log(event.currentTarget);
-                if($(event.currentTarget).hasClass("btn-primary")) {
+                if($(event.currentTarget).hasClass("btn-primary") && event.target.id !== "step5") {
                     var currentStep = parseInt(event.currentTarget.id.replace("step", ""), 10) - 1;
                     console.log(currentStep);
                     var nextStep = currentStep + 1;
@@ -123,9 +147,19 @@
                     $("ul.form li").eq(currentStep).addClass("hidden");
                     $("ul.form li").eq(nextStep).removeClass("hidden");
                     $("ul.form li").eq(nextStep).fadeIn("slow");
+                    $("#progress_bar").css("width", ((nextStep / 5) * 100) + "%");
                 }else if(event.currentTarget.id === "update_button") {
                     alert("Coming Soon");
+                }else if(event.target.id === "step5") {
+                    // Run AJAX - Delete Setup file - TODO
+                    window.location.href = "/login";
                 }
+            });
+            $("#connection_check").click(function() {
+                // Run AJAX - TODO
+            });
+            $("#step5_submit").click(function() {
+                // Run AJAX - TODO
             });
         </script>
     </body>
