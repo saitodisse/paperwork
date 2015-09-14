cb69c59a7b11b66c233a6a187af17c9b648a3c3b
Felipe Arenales <fearenales@gmail.com>
2015-09-14 19:11:20 -0300

    Replacing process.env to env in Azkfile.js

--- Azkfile.js
+++ Azkfile.js
@@ -44,7 +44,7 @@ systems({
     http: {
       domains: [
         "paperwork.#{azk.default_domain}", // default azk
-        "#{process.env.AZK_HOST_IP}"       // used if deployed
+        "#{env.AZK_HOST_IP}"               // used if deployed
       ]
     },
     ports: {
@@ -98,7 +98,7 @@ systems({
       // that way you can connect with:
       // $ ssh git@REMOTE.IP
       // $ bash
-      "/azk/deploy/.ssh": path("#{process.env.HOME}/.ssh")
+      "/azk/deploy/.ssh": path("#{env.HOME}/.ssh")
     },
 
     // this is not a server
